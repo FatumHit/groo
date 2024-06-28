b67924c2f61c5576d69dc23ea7494c32172449f1
FatumHit <71377942+FatumHit@users.noreply.github.com>
2024-06-28 14:55:50 +0300

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,10 @@
-def testPipe(sparta){
-  println"This is ${sparta}"
+class Example {
+   static def DisplayName() {
+      println("This is how methods work in groovy");
+      println("This is an example of a simple method");
+   } 
+	
+   static def lastName() {
+      println("Lo-Lo-Lo");
+   } 
 }
