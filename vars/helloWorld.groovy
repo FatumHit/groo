1bf80728d7480b8a557398f7509daa9cac8a5c7f
FatumHit <71377942+FatumHit@users.noreply.github.com>
2024-06-28 15:00:18 +0300

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,10 +1,11 @@
-class printNames {
-   static def firstName() {
-      println("This is how methods work in groovy");
-      println("This is an example of a simple method");
-   } 
-	
-   static def lastName() {
-      println("Lo-Lo-Lo");
-   } 
+def sayHello() {
+    println "Hello"
 }
+
+class MyClass {
+    MyClass(Script host) {
+        host.sayHello()
+    }
+}
+
+def a = new MyClass(this)
